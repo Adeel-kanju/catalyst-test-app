621a18d82921a0c947885c2bc600f0170cd28022
Adeel-kanju <adeel.kanju114@gmail.com>
2023-01-08 17:37:04 +0500

    Module 3 add state

--- src/Context.js
+++ src/Context.js
@@ -1,10 +1,11 @@
-import React from "react"
+import React, {useState} from "react"
 
 const Context = React.createContext()
 
 function ContextProvider({children}) {
+    const [allPhotos, setAllPhotos] = useState([])
     return (
-        <Context.Provider value="">
+        <Context.Provider value={{allPhotos}}>
             {children}
         </Context.Provider>
     )
