741c3a64f10aae46cae24aa8a9c4bd7ef354fbff
Adeel-kanju <adeel.kanju114@gmail.com>
2023-01-08 23:08:20 +0500

    module 14

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,23 @@
-import React from "react"
+import React, { useContext } from "react"
+import { Context } from "../Context"
+import CartItem from "../components/Cartitem"
+
 
 function Cart() {
+    const { cartItems } = useContext(Context)
+    const cartItemElements = cartItems.map(item => (
+        <CartItem key={item.id} item={item} />
+    ))
+    const totalCost = (5.99 * cartItems.length).toLocaleString("en-US", {style: "currency", currency: "USD"})
+    
     return (
         <main className="cart-page">
             <h1>Check out</h1>
+            {cartItemElements}
+            <p className="total-cost">Total:{totalCost} </p>
+            <div className="order-button">
+                <button>Place Order</button>
+            </div>
         </main>
     )
 }
