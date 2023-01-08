c416205e098b3747a8127eaa9ec824f7edebe4bd
Adeel-kanju <adeel.kanju114@gmail.com>
2023-01-08 17:45:23 +0500

    Module 4

--- src/Context.js
+++ src/Context.js
@@ -1,9 +1,17 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 
 const Context = React.createContext()
 
 function ContextProvider({children}) {
     const [allPhotos, setAllPhotos] = useState([])
+    const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
+    useEffect(() => {
+        // fetch data and save data 
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setAllPhotos(data))
+    }, [])
+    
     return (
         <Context.Provider value={{allPhotos}}>
             {children}
